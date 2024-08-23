3df6a3ebc6c0f67f88cffddde6fb1f43b35b7312
Jack <jack222guerrero@hotmail.com>
2024-08-22 21:01:00 -0500

    00Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Jack-DanielsPy.py
@@ -0,0 +1,30 @@
+# web oficial de Python https://www.python.org/
+
+# Comentario
+
+"""
+Estos es un comentario en varias lineas
+
+"""
+
+'''
+Esto tambien es un comentario
+'''
+
+#variable
+
+mi_variable = "mi variable"
+
+entero = 10
+
+float = 10.5
+
+boolenao = True
+
+
+cadenatexto = "hola que tal"
+
+print("Hola [Python]!")
+print(type(entero))
+print(type(float))
+print(type(boolenao))
